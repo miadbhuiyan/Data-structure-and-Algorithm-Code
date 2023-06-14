313399289746f0785f2118d73918772afebc145e
Miad Bhuiyan <124463760+miadbhuiyan@users.noreply.github.com>
2023-06-14 13:40:53 -0500

    Add files via upload

---
+++ Accessing_Element_of_Array.py
@@ -0,0 +1,17 @@
+# importing array module
+import array as arr
+
+# array with int type
+a= arr.array('i',[1,2,3,4,5])
+
+# accessing element of array
+print("Access element is: ",a[0])
+
+# accessing element of array
+print("Access element is: ",a[3])
+
+
+# accessing element of array
+print("Access element is: ",a[4])
+
+#Time and space complaxity of this program is O(1)
