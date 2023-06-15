11a9224ccef6fcba4cff8728c9db0274a07b1bed
Miad Bhuiyan <124463760+miadbhuiyan@users.noreply.github.com>
2023-06-15 05:50:30 -0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,11 @@
+String = "Welcome to GeeksForGeeks"
+print("Creating String: ")
+print(String)
+
+# Printing First character
+print("\nFirst character of String is: ")
+print(String[0])
+
+# Printing Last character
+print("\nLast character of String is: ")
+print(String[-1])
